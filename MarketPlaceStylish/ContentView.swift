b8e5608a5657a953d0d7669c9a0ca085987a79a3
SilasBorges <silasborgesc@hotmail.com>
2026-07-18 21:04:16 -0300

    Initial Commit

---
+++ MarketPlaceStylish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarketPlaceStylish
+//
+//  Created by Silas Borges on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarketPlaceStylish/MarketPlaceStylishApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPlaceStylishApp.swift
+//  MarketPlaceStylish
+//
+//  Created by Silas Borges on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MarketPlaceStylishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
