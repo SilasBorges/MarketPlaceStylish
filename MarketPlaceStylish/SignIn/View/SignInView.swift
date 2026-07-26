cb8d513801941af3207012d2823e474825220d54
SilasBorges <silasborgesc@hotmail.com>
2026-07-26 20:10:43 -0300

    feat - view SignIn

---
+++ MarketPlaceStylish/SignIn/View/SignInView.swift
@@ -0,0 +1,34 @@
+//
+//  SignInView.swift
+//  MarketPlaceStylish
+//
+//  Created by Silas Borges on 24/07/26.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .center) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Welcome \nBack!")
+                        .font(.system(size: 36).bold())
+                    
+                    EditTextView(text: .constant("Username or Email"), icon: "person.fill", placeholder: "Email")
+                        .padding(.top, 24)
+                    
+                    EditTextView(text: .constant("Password"), icon: "lock.fill",
+                        placeholder: "Password")
+                        .padding(.top, 16)
+                    
+                }
+            }.padding()
+        }
+    }
+}
+
+#Preview {
+    SignInView()
+}
